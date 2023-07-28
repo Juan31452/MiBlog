2201e7dd6e4ab69cb40b3a5830123a9207dc9b50
Juan Carlos <jcs1011@gmail.com>
2023-07-28 18:33:48 -0500

    se agrega control de errores

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import app from "./app.js"
-
 const port = 3000;
 
 //http://localhost:3000
@@ -18,7 +17,26 @@ app.get("/usuario/:id", (req, res) => {
   res.send(`Hola, usuario ${userId}!`);
 });
 
+// Middleware para manejar errores 404 (Not Found)
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+}); 
+
+// Middleware de manejo de errores
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      status: err.status || 500,
+      message: err.message || 'Internal Server Error',
+    },
+  });
+});
+
 // Iniciar el servidor y escuchar en el puerto especificado
 app.listen(port, () => {
     console.log(`Conectado en el puerto:${port}`);
   });
+
